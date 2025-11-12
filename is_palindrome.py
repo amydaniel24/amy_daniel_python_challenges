e303019442262cf5f5af11c8105e243b03fd54e5
Amy Daniel <amymchargue@ymail.com>
2025-11-12 10:43:02 -0500

    Added print statements and ran

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,5 +1,9 @@
 # Python Challenge : Is it a Palindrome?
 def is_palindrome(s):
-    text = text.lower()
-    reversed_text = text[::-1]
-    return text == reversed_text
+    s = s.lower()
+    reversed_text = s[::-1]
+    return s == reversed_text
+
+print(is_palindrome("racecar"))  
+print(is_palindrome("hello"))     
+print(is_palindrome("madam"))     

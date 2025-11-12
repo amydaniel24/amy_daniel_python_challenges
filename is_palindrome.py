ef076094bf9f6048546bb50a0cbae38149df7813
Amy Daniel <amymchargue@ymail.com>
2025-11-12 10:53:40 -0500

    Normalized the string

--- is_palindrome.py
+++ is_palindrome.py
@@ -7,3 +7,23 @@ def is_palindrome(s):
 print(is_palindrome("racecar"))  
 print(is_palindrome("hello"))     
 print(is_palindrome("madam"))     
+
+# Normalizing it (convert to lower case, keep only letters and numbers, check for palindrome)
+def is_palindrome_clean(s):
+    
+
+    cleaned = ""  
+    for char in s:
+        if char.isalnum():  
+            cleaned += char.lower()
+
+    
+    return cleaned == cleaned[::-1]
+
+
+
+print(is_palindrome_clean("Racecar"))                    
+print(is_palindrome_clean("A man a plan a canal Panama")) 
+print(is_palindrome_clean("Hello, world!"))           
+
+

2eb87ae7a62a6c052c6d8beca5cce523cb1da2cc
Amy Daniel <amymchargue@ymail.com>
2025-11-12 11:18:58 -0500

    add function to return max num and index

--- find_max.py
+++ find_max.py
@@ -16,3 +16,21 @@ print(find_max([4, 9, 1, 17, 2]))
 print(find_max([-5, -9, -2, -12]))   
 print(find_max([42]))                 
 print(find_max([]))                  
+
+# Return the maximum number and its index
+def find_max_with_index(numbers):
+    if not numbers:
+        return None, None
+    
+    max_value = numbers[0]
+    max_index = 0
+
+    for i in range(len(numbers)):
+        if numbers[i] > max_value:
+            max_value = numbers[i]
+            max_index = i
+
+    return max_value, max_index
+
+print(find_max_with_index([4, 9, 1, 17, 2]))  
+print(find_max_with_index([-5, -9, -2, -12])) 

36d97fdc672cf73b45d1076280a49a5c8639fb99
Amy Daniel <amymchargue@ymail.com>
2025-11-12 11:37:48 -0500

    added function to check up to the square root of n

--- is_prime.py
+++ is_prime.py
@@ -14,3 +14,20 @@ print(is_prime(11))
 print(is_prime(15))  
 print(is_prime(1))   
 print(is_prime(0))   
+
+#Bonus functions 
+import math
+
+def is_prime_efficient(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime_efficient(2))    
+print(is_prime_efficient(11))  
+print(is_prime_efficient(15))  
+print(is_prime_efficient(97))   
+

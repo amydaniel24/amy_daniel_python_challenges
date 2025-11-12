cb34ddc460f6b93fa7514149d588bac8b7d2d3b6
Amy Daniel <amymchargue@ymail.com>
2025-11-12 11:31:53 -0500

    define function is_prime

---
+++ is_prime.py
@@ -0,0 +1,16 @@
+# Python Challange: Check if a Number is Prime  
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+print(is_prime(2))  
+print(is_prime(11))  
+print(is_prime(15))  
+print(is_prime(1))   
+print(is_prime(0))   

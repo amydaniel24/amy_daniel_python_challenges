68af4db7db2d047f269a35ff52094749addd1560
Amy Daniel <amymchargue@ymail.com>
2025-11-13 08:29:35 -0500

    defined function check_password_strength

--- password_strength.py
+++ password_strength.py
@@ -1,1 +1,25 @@
 # LEVEL UP! Bonus Challenge
+# Challenge: Password Strength Checker
+
+
+
+def check_password_strength(password):
+    
+    length = len(password) >= 8
+    upper = any(c.isupper() for c in password)
+    lower = any(c.islower() for c in password)
+    digit = any(c.isdigit() for c in password)
+    symbol = any(not c.isalnum() for c in password)
+
+    score = sum([length, upper, lower, digit, symbol])
+
+    if score == 5:
+        return "Strong "
+    elif 3 <= score < 5:
+        return "Medium "
+    else:
+        return "Weak "
+
+
+password = input("Enter a password: ")
+print("Strength:", check_password_strength(password))

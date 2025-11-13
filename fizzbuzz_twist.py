219e650ba2f0cf3a15cd539766e7d5e14a71f7e3
Amy Daniel <amymchargue@ymail.com>
2025-11-13 08:37:30 -0500

    defined function fizzbuzz_twist

--- fizzbuzz_twist.py
+++ fizzbuzz_twist.py
@@ -1,3 +1,20 @@
 # LEVEL UP! Bonus Challenge
 # Challenge: FizzBuzz with a Twist 
 
+
+def fizzbuzz_twist():
+    
+    for num in range(1, 101):
+        result = ""
+        if num % 3 == 0:
+            result += "Fizz"
+        if num % 5 == 0:
+            result += "Buzz"
+        if num % 7 == 0:
+            result += "Bang"
+
+      
+        print(result or num)
+
+
+fizzbuzz_twist()
